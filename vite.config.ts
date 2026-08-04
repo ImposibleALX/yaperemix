c8211055f55ce736388ba3259458fbd144c0304f
ImposibleALX <soyimposibleyt@hotmail.com>
2026-08-04 07:10:12 -0500

    a

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     base: './',
+    build: {
+      target: 'es2015',
+    },
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
